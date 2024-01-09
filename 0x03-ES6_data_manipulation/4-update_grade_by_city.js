e0871712b6597ebfcbcde07810d9bcac4c3a2f23
chaymaeedd <c.eddouks@gmail.com>
2024-01-09 05:55:10 -0800

    Task4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,21 +2,21 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   if (!Array.isArray(students) || !Array.isArray(newGrades)) {
     return [];
   }
-	// Filter students based on their location
-	// Update or add grades for students in the newGrades list
-	// If a student from the filtered list is not found in newGrades, set their grade to N/A
-	const cityStudents = students.filter((student) => student.location === city).map((student) => {
+  // Filter students by location
+  // Change/add grade for students in newGrades
+  // If a filtered student is not in new Grades => grade = N/A
+
+  const cityStudents = students.filter((student) => student.location === city).map((student) => {
     const item2 = newGrades.find((student2) => student.id === student2.studentId);
-		// });
-		// console.log('FOUND STUDENT1: ', student);
-		// console.log('FOUND ITEM2: ', item2);
-		// console.log(item2 ? {...student, ...item2} : student.grade = 'N/A', student);
-		// If item2 exists, merge its properties with the student object; otherwise, set student's grade to 'N/A'
-		// if (!item2) {
-		// student.grade = 'N/A'; // eslint-disable-line no-param-reassign
-		// }
-		return { ...student, grade: item2 ? item2.grade : 'N/A' };
+    // });
+    // console.log('FOUND STUDENT1: ', student);
+    // console.log('FOUND ITEM2: ', item2);
+    // console.log(item2 ? {...student, ...item2} : student.grade = 'N/A', student);
+    // if (!item2) {
+    //   student.grade = 'N/A'; // eslint-disable-line no-param-reassign
+    // }
+    return { ...student, grade: item2 ? item2.grade : 'N/A' };
     // return item2 ? {...student, ...item2} : student,  student.grade = 'M/A';
-		});
+  });
   return cityStudents;
 }
